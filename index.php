b1c2ab8d76389adc49d4d527d73c55a85a80a84e
julianafreiitas <juliana.freitasmk2@gmail.com>
2026-04-12 17:13:04 -0300

    restante

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+
+<body>
+    <div class="imagem"></div>
+    <form class="formulario">
+        <h1>Login</h1>
+        <div class="input_grup">
+            <h2>Nome</h2>
+            <input class="campo" type="text">
+        </div>
+
+        <div class="input_grup">
+            <h2>Senha</h2>
+            <input class="campo" type="password">
+        </div>
+
+        <button class="botao">Entrar</button>
+     
+
+</form>
+
+
+</body>
+
+</html>
