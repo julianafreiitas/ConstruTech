25635a3bc3017ba543395216580a4bf6e72cc71f
julianafreiitas <juliana.freitasmk2@gmail.com>
2026-04-19 15:09:17 -0300

    ola

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,8 @@
  <header class="left">
         <img src="./imagens/logo.png" alt="logo">
         <nav class="leftbtn">
-            <a href="produtos.php">Produto</a>
+            <a href="produtos.php">Produtos</a>
             <a href="cadastro.php">Cadastro</a>
+            <a href="cadastro.php">Atualizar</a>
         </nav>
     </header>
--- produtos.php
+++ produtos.php
@@ -2,19 +2,19 @@
 require_once 'init.php';
 $categoria_get = isset($_GET['categoria']) ? trim($_GET['categoria']) : '';
 
-if (isset($_GET['excluir'])){
+if (isset($_GET['excluir'])) {
     $id = $_GET['excluir'];
-    if (isset($_SESSION['produtos'])){
-        foreach ($_SESSION['produtos'] as $posicao => $produto ){
-        if ( $produto['codigo_produto'] == $id){
-            unset($_SESSION['produtos'][$posicao]);
-            break;
+    if (isset($_SESSION['produtos'])) {
+        foreach ($_SESSION['produtos'] as $posicao => $produto) {
+            if ($produto['codigo_produto'] == $id) {
+                unset($_SESSION['produtos'][$posicao]);
+                break;
+            }
         }
-    }
-    $_SESSION['produtos'] = array_values($_SESSION['produtos']);
+        $_SESSION['produtos'] = array_values($_SESSION['produtos']);
 
-    header("Location: produtos.php");
-    exit;
+        header("Location: produtos.php");
+        exit;
     }
 }
 ?>
@@ -30,22 +30,22 @@ if (isset($_GET['excluir'])){
 </head>
 
 <body>
-   <?php
-   require_once 'partials/header.php';
+    <?php
+    require_once 'partials/header.php';
 
 
-   ?>
+    ?>
     <main>
         <div class="topo">
             <div class="catalogos">
                 <h1>Produtos</h1>
                 <?php
-                    echo '<ul class="catalogo">';
-                    echo'<li><img src="./imagens/filtro.png" alt="filtro"></li>';
-                    foreach ($categorias as $kcat => $nome) {
-                    echo'<li><a href="produtos.php?categoria='.$kcat.'">'.$nome.'</a></li>';
-                    }
-                echo'</ul>';
+                echo '<ul class="catalogo">';
+                echo '<li><img src="./imagens/filtro.png" alt="filtro"></li>';
+                foreach ($categorias as $kcat => $nome) {
+                    echo '<li><a href="produtos.php?categoria=' . $kcat . '">' . $nome . '</a></li>';
+                }
+                echo '</ul>';
                 ?>
             </div>
             <div class="tb_estoque">
@@ -72,46 +72,54 @@ if (isset($_GET['excluir'])){
                 <th>Detalhes</th>
             </tr>
 
-            <?php foreach ($_SESSION['produtos'] as $produto): ?>
+            <?php
+            $totalGerado = 0;
+            foreach ($_SESSION['produtos'] as $produto): 
+            ?>
 
-                 <?php if ($categoria_get == '' || $produto['categoria'] === $categoria_get): ?>
-                <tr>
-                    <td>
-                        <div>
-                           
-                            <img src="<?php echo $produto['imagem']; ?>">
-                            <p><?php echo $produto['nome']; ?></p>
-                        </div>
-                    </td>
-                    <td><?php echo $produto['categoria']; ?></td>
-                    <td>R$ <?php echo $produto['preco']; ?></td>
-                    <td>
-                        <?php 
-                        if ($produto['quantidade'] >= 100){
-                            echo "<img src='./estado/verde.png'";
-                        }
-                        elseif ($produto['quantidade'] <= 99 && $produto['quantidade'] >= 50){
-                            echo "<img src='./estado/amarelo.png'";
-                        }
+                <?php 
+                if ($categoria_get == '' || $produto['categoria'] === $categoria_get):
+                    $valorItem = $produto['quantidade'] * $produto['preco'];
+                    $totalGerado += $valorItem;
+                 ?>
+                    <tr>
+                        <td>
+                            <div>
 
-                        else {
-                            echo "<img src='./estado/vermelho.png'";
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <a href='detalhes.php?id=<?php echo $produto['codigo_produto']; ?>' class='btn'>Ver mais</a>
-                    </td>
-                </tr>
+                                <img src="<?php echo $produto['imagem']; ?>">
+                                <p><?php echo $produto['nome']; ?></p>
+                            </div>
+                        </td>
+                        <td><?php echo $produto['categoria']; ?></td>
+                        <td>R$ <?php echo $produto['preco']; ?></td>
+                        <td>
+                            <?php
+                            if ($produto['quantidade'] >= 100) {
+                                echo "<img src='./estado/verde.png'";
+                            } elseif ($produto['quantidade'] <= 99 && $produto['quantidade'] >= 50) {
+                                echo "<img src='./estado/amarelo.png'";
+                            } else {
+                                echo "<img src='./estado/vermelho.png'";
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <a href='detalhes.php?id=<?php echo $produto['codigo_produto']; ?>' class='btn'>Ver mais</a>
+                        </td>
+                    </tr>
                 <?php endif; ?>
             <?php endforeach; ?>
-            <!-- <td>Bruto</td>
-            <td>R$95.00</td>
-            <td>Disponível</td>
-            <td><button href="#" class="btn">Ver mais</button></td>
-            </tr> -->
         </table>
+        <footer>
+            <div class="rodape">
+                <div class="total">
+                    <h1>Total em Estoque: R$ <?php echo number_format($totalGerado, 2,',','.' ); ?></h1>
+                </div>
+            </div>
+        </footer>
     </main>
+
+
 
 </body>
 
