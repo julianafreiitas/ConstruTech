fdba322d6b47d44c3ae4d48b9a124300b2a78f1b
NICOLE HANAI HATAKEYAMA <25254383@educ123.sp.senai.br>
2026-04-22 09:34:08 -0300

    Acesso na página

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link rel="stylesheet" href="./css/login.css">
+    <script>
+        function test(){
+            const usuario = document.querySelector("#user").value;
+            const senha = document.querySelector("#pass").value;
+
+            if(usuario === "juliana" && senha ==="123") {
+                window.location.href= "./produtos.php";
+            }
+            else if (usuario === "Juliana" && senha ==="123") {
+                window.location.href= "./produtos.php";
+            }
+            else{
+                alert("Acesso negado! Digite direito >:(");
+            }
+        }     
+    </script>
 </head>
 
 <body>
@@ -14,17 +30,17 @@
         <h1>Bem-vindo ao Sistema</h1>
         <div class="input_grup">
             <h2>Nome</h2>
-            <input class="campo" type="text">
+            <input class="campo" type="text" id="user">
         </div>
 
         <div class="input_grup">
             <h2>Senha</h2>
-            <input class="campo" type="password">
+            <input class="campo" type="password" id="pass">
         </div>
 
-        <a href="produtos.php" class="botao">Entrar</a>
-     
+        <!-- href="produtos.php" -->
 
+        <button class="botao" onclick="test()" type="button">Entrar</button>
 </form>
 
 
