825ee3aedb101077732465e8fb4904956742cabb
julianafreiitas <juliana.freitasmk2@gmail.com>
2026-04-21 21:30:21 -0300

    Refactor forms and styles in cadastro and atualizacao pages for improved layout and usability

--- atualizacao.php
+++ atualizacao.php
@@ -9,8 +9,8 @@ require_once 'init.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/cadastro.css">
     <link rel="stylesheet" href="./css/produto.css">
+    <link rel="stylesheet" href="./css/cadastro.css">
     <link rel="stylesheet" href="./css/atualizar.css">
 </head>
 
@@ -18,24 +18,83 @@ require_once 'init.php';
     <?php
     require_once 'partials/header.php';
     ?>
+    <div class="conteudo">
 
-    <div class="linha_top">
-        <h1>Atualizar/Editar</h1>
+        <div class="linha_top">
+            <h1>Atualizar/Editar</h1>
+        </div>
+        <div class="centro">
+            <div class="quadrado">
 
-        <div class="quadrado"></div>
+
+                <form class="form" method="POST">
+
+
+                    <div class="linha">
+                        <div class="infor">
+                            <h2 class="titulo">Nome</h2>
+                            <input class="campo" type="text" name="nome">
+                        </div>
+
+                        <div class="infor">
+                            <h2 class="titulo_p">Preço</h2>
+                            <input class="campo_p" type="text" name="preco">
+                        </div>
+
+                        <div class="infor">
+                            <h2 class="titulo">Qtd.</h2>
+                            <input class="campo_quantidade" type="text" name="quantidade">
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="linha_2">
+
+                        <div>
+
+                            <div>
+                                <h2 class="titulo">Categoria</h2>
+
+                                <select name="categoria" class="lista">
+                                    <option value="bruto">Selecione a categotia...</option>
+                                    <option value="bruto">Bruto</option>
+                                    <option value="ferramentas">Ferramentas</option>
+                                    <option value="acabamento">Acabamento</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <input type="text" name="imagem" placeholder="URL da imagem">
+
+
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <h2 class="titulo_descricao">Descrição</h2>
+                            <textarea class="campo_descricao" name="descricao"></textarea>
+                        </div>
+
+                    </div>
+
+                    <div class="linha">
+                        <button type="submit" class="botao_cadastrar">Cadastrar</button>
+                    </div>
+
+                </form>
+
+
+            </div>
+
+
+
+        </div>
     </div>
 
-    
-
+    </div>
 </body>
 
 </html>
-
-
-
-
-
-  
-
-
-               
--- cadastro.php
+++ cadastro.php
@@ -9,9 +9,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_SESSION['produtos'][] = [
         'codigo_produto' => $novoCodigo,
         'nome' => $_POST['nome'],
-        'preco' => (float)$_POST['preco'],
+        'preco' => (float) $_POST['preco'],
         'categoria' => $_POST['categoria'],
-        'quantidade' => (int)$_POST['quantidade'],
+        'quantidade' => (int) $_POST['quantidade'],
         'qtd_minima' => 1,
         'qtd_maxima' => 100,
         'descricao' => $_POST['descricao'],
@@ -39,80 +39,78 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ?>
 
     <div class="conteudo">
-         <div class="linha_1">
+        <div class="linha_1">
             <h1>Cadastro de produtos</h1>
+        </div>
+        <div class="centro">
+            <div class="quadrado">
+
+
+                <form class="form" method="POST">
+
+
+                    <div class="linha">
+                        <div class="infor">
+                            <h2 class="titulo">Nome</h2>
+                            <input class="campo" type="text" name="nome">
+                        </div>
+
+                        <div class="infor">
+                            <h2 class="titulo_p">Preço</h2>
+                            <input class="campo_p" type="text" name="preco">
+                        </div>
+
+                        <div class="infor">
+                            <h2 class="titulo">Qtd.</h2>
+                            <input class="campo_quantidade" type="text" name="quantidade">
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="linha_2">
+
+                        <div>
+
+                            <div>
+                                <h2 class="titulo">Categoria</h2>
+
+                                <select name="categoria" class="lista">
+                                    <option value="bruto">Selecione a categotia...</option>
+                                    <option value="bruto">Bruto</option>
+                                    <option value="ferramentas">Ferramentas</option>
+                                    <option value="acabamento">Acabamento</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <input type="text" name="imagem" placeholder="URL da imagem">
+
+
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <h2 class="titulo_descricao">Descrição</h2>
+                            <textarea class="campo_descricao" name="descricao"></textarea>
+                        </div>
+
+                    </div>
+
+                    <div class="linha">
+                        <button type="submit" class="botao_cadastrar">Cadastrar</button>
+                    </div>
+
+                </form>
+
+
             </div>
-        <div class="quadrado">
-           
 
-            <form method="POST">
-
-
-                <div class="linha">
-                    <div class="infor">
-                        <h2 class="titulo">Nome</h2>
-                        <input class="campo" type="text" name="nome">
-                    </div>
-
-                    <div class="infor">
-                        <h2 class="titulo_p">Preço</h2>
-                        <input class="campo_p" type="text" name="preco">
-                    </div>
-
-                    <div class="infor">
-                        <h2 class="titulo">Qtd.</h2>
-                        <input class="campo_quantidade" type="text" name="quantidade">
-                    </div>
-                </div>
-
-
-
-
-                <div class="linha">
-
-                    <div>
-
-                        <div>
-                            <h2 class="titulo">Categoria</h2>
-
-                            <select name="categoria" class="lista"> <option value="bruto">Selecione a categotia...</option>
-                                <option value="bruto">Bruto</option>
-                                <option value="ferramentas">Ferramentas</option>
-                                <option value="acabamento">Acabamento</option>
-                            </select>
-                        </div>
-
-                        <div>
-
-                             <label for="file-upload" class="custom-file-upload">
-                                <img class="img" src="./imagens/uplaod.png" alt="">
-                                <i class="fa fa-cloud-upload"></i> Selecionar Imagem
-                            </label>
-                            <input type="text" name="imagem" placeholder="URL da imagem">
-
-
-                        </div>
-
-                    </div>
-
-                    <div>
-                        <h2 class="titulo_descricao">Descrição</h2>
-                        <textarea class="campo_descricao" name="descricao"></textarea>
-                    </div>
-
-                </div>
-
-                <div class="linha">
-                    <button type="submit" class="botao_cadastrar">Cadastrar</button>
-                </div>
-
-            </form>
 
 
         </div>
-
-
-
     </div>
 
 
